f1b94db48fbf7d606217fa431c0873e28cd44e53
Daniel-spd <90364886+Daniel-spd@users.noreply.github.com>
2021-09-16 21:38:01 -0500

    Create necesario.js

---
+++ necesario.js
@@ -0,0 +1,15 @@
+
+function mostrarCedula(){
+	var variable_cedula = document.getElementById("cedula").value;
+	alert(variable_cedula);
+}
+
+function mostrarNombre(){
+	var variable_nombre = document.getElementById("nombre").value;
+	alert(variable_nombre);
+}
+
+function mostrarApellido(){
+	var variable_apellido = document.getElementById("apellido").value;
+	alert(variable_apellido);
+}
