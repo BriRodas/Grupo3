9a89b3ba9ad7dea1cf781f8330e383fa49dd8a32
Daniel-spd <90364886+Daniel-spd@users.noreply.github.com>
2021-09-27 22:23:47 -0500

    Create funciones.js

---
+++ Sprint 3/funciones.js
@@ -0,0 +1,28 @@
+function obtenerdatos(){
+	var cedula = document.getElementById("cedula").value;
+	var apellido = document.getElementById("apellido").value;
+	var nombre = document.getElementById("nombre").value;
+	var edad = document.getElementById("edad").value;
+	var peso = document.getElementById("peso").value;
+	var talla = document.getElementById("talla").value;
+
+	edad1 = parseInt(edad);
+
+	a = parseFloat(peso);
+
+	b = parseFloat(talla);
+
+	c = a / (b**2);
+
+	document.form.cedulaobtenido.value = cedula;
+	document.form.apellidoobtenido.value = apellido;
+	document.form.nombreobtenido.value = nombre;
+	document.form.edadobtenido.value = edad1;
+	document.form.pesoobtenido.value = a;
+	document.form.tallaobtenido.value = b;
+	document.form.imcobtenido.value = c;
+}
+
+function boton(){
+	alert("Hola Seño");
+}
